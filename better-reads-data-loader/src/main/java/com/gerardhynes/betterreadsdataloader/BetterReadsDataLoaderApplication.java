3c18631de76268c654d92df86ffe20266ad5cb2c
Gerard Hynes <gkhynes@gmail.com>
2022-01-06 13:38:34 +0000

    Create author instance

--- better-reads-data-loader/src/main/java/com/gerardhynes/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ better-reads-data-loader/src/main/java/com/gerardhynes/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -2,20 +2,39 @@ package com.gerardhynes.betterreadsdataloader;
 
 import java.nio.file.Path;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-import connection.DataStaxAstraProperties;
+import com.gerardhynes.betterreadsdataloader.author.Author;
+import com.gerardhynes.betterreadsdataloader.author.AuthorRepository;
+import com.gerardhynes.betterreadsdataloader.connection.DataStaxAstraProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadsDataLoaderApplication {
 
+	@Autowired AuthorRepository authorRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+	}
+
+	@PostConstruct
+	public void start(){
+		System.out.println("Application started");
+
+		Author author = new Author();
+		author.setId("id");
+		author.setName("name");
+		author.setPersonalName("personalName");
+
+		authorRepository.save(author);
 	}
 
 	@Bean
