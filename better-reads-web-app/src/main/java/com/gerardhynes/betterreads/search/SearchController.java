7a76ac3a3294fd1a7816a1defcf58d1987dc2033
Gerard Hynes <gkhynes@gmail.com>
2022-01-13 22:20:45 +0000

    Get search results displaying

--- better-reads-web-app/src/main/java/com/gerardhynes/betterreads/search/SearchController.java
+++ better-reads-web-app/src/main/java/com/gerardhynes/betterreads/search/SearchController.java
@@ -1,12 +1,12 @@
 package com.gerardhynes.betterreads.search;
 
-import com.datastax.dse.driver.api.core.graph.predicates.Search;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -14,6 +14,8 @@ import java.util.stream.Collectors;
 
 @Controller
 public class SearchController {
+    private final String COVER_IMAGE_ROOT= "http://covers.openlibrary.org/b/id/";
+
     private final WebClient webClient;
 
     public SearchController(WebClient.Builder webClientBuilder){
@@ -33,6 +35,17 @@ public class SearchController {
                 List<SearchResultBook> books = result.getDocs()
                         .stream()
                         .limit(10)
+                        .map(bookResult -> {
+                            bookResult.setKey(bookResult.getKey().replace("/works/", ""));
+                            String coverId = bookResult.getCover_i();
+                            if(StringUtils.hasText(coverId)){
+                                coverId = COVER_IMAGE_ROOT + coverId + "-M.jpg";
+                            } else {
+                                coverId = "/images/no-image.png";
+                            }
+                            bookResult.setCover_i((coverId));
+                            return bookResult;
+                        })
                         .collect(Collectors.toList());
                 model.addAttribute("searchResults", books);
         return "search";
