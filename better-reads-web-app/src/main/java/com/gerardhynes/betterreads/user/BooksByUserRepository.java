7808a3398d6e42509eb481391aca716a7c119a67
Gerard Hynes <gkhynes@gmail.com>
2022-01-19 12:33:01 +0000

    Show books on user homepage

--- better-reads-web-app/src/main/java/com/gerardhynes/betterreads/user/BooksByUserRepository.java
+++ better-reads-web-app/src/main/java/com/gerardhynes/betterreads/user/BooksByUserRepository.java
@@ -1,9 +1,9 @@
 package com.gerardhynes.betterreads.user;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
-import java.awt.print.Pageable;
 
 public interface BooksByUserRepository extends CassandraRepository<BooksByUser, String> {
     Slice<BooksByUser> findAllById(String id, Pageable pageable);
